698720a71042cfaa6087806228f9fe3559daaf10
Takuya Yamauchi <tyam@office.amsiag.com>
2016-04-07 13:56:21 +0900

    Updated.

--- mnist00.py
+++ mnist00.py
@@ -20,4 +20,6 @@ for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-#correct_prediction = tf.equal(tf.equal(tf.argmax(y, 1), tf.argmax(
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
