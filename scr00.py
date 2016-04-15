a03320939b6805ae0259bd0fa61ed79044fee5b0
Takuya Yamauchi <tyam@office.amsiag.com>
2016-04-15 14:23:19 +0900

    - Updated.

--- scr00.py
+++ scr00.py
@@ -2,11 +2,19 @@ import tensorflow as tf
 
 a = tf.Variable(tf.zeros([1]))
 b = tf.Variable(1)
+c = tf.Variable(tf.constant([0, 1], dtype=tf.float32))
+d = tf.constant([0, 1], dtype=tf.float32)
 
 init = tf.initialize_all_variables()
 
+
 sess = tf.Session()
 
+print sess.run(d)
 sess.run(init)
 print sess.run(a)
+print sess.run(b)
+print sess.run(c)
+
+sess.run(b.assign(2))
 print sess.run(b)
