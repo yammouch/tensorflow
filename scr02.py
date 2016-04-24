b02eb4fe2464d4afcf02a30430597c1dc23480b2
Takuya Yamauchi <yammouch@gmail.com>
2016-04-24 19:24:01 +0900

    - Updated.

--- scr02.py
+++ scr02.py
@@ -1,12 +1,17 @@
+import numpy as np
 import tensorflow as tf
 
 a = tf.Variable(tf.constant(0.0, dtype=tf.float32))
 b = tf.Variable(tf.constant(0.0, dtype=tf.float32))
-x = tf.constant([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=tf.float32)
-y_ = tf.constant([-3.0, -1.0, 1.0, 3.0, 5.0], dtype=tf.float32)
+#x = tf.constant([-2.0, -1.0, 0.0, 1.0, 2.0], dtype=tf.float32)
+x = tf.placeholder(tf.float32, [5])
+xval = np.array([-2, -1, 0, 1, 2], dtype=np.float32)
+#y_ = tf.constant([-3.0, -1.0, 1.0, 3.0, 5.0], dtype=tf.float32)
+y_ = tf.placeholder(tf.float32, [5])
+y_val = np.array([-3, -1, 1, 3, 5], dtype=np.float32)
 y = a*x + b
 
-error = (y - y_)**2
+error = tf.reduce_sum((y - y_)**2)
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(error)
 
 init = tf.initialize_all_variables()
@@ -14,6 +19,6 @@ init = tf.initialize_all_variables()
 with tf.Session() as sess:
   sess.run(init)
   print "a: %f, b: %f" % (sess.run(a), sess.run(b))
-  for i in range(50):
-    sess.run(train_step)
+  for i in range(10):
+    sess.run(train_step, feed_dict={x: xval, y_: y_val})
     print "a: %f, b: %f" % (sess.run(a), sess.run(b))
